411bffe441215ca64fa6f3e6ff9b4ca6fba0fee1
Justin Newberry <justin@comma.ai>
2024-02-03 11:52:05 -0800

    wip

--- system/hardware/tici/tests/test_power_draw.py
+++ system/hardware/tici/tests/test_power_draw.py
@@ -15,7 +15,9 @@ from openpilot.system.hardware.tici.power_monitor import get_power
 from openpilot.selfdrive.manager.process_config import managed_processes
 from openpilot.selfdrive.manager.manager import manager_cleanup
 
-SAMPLE_TIME = 8   # seconds to sample power
+SAMPLE_TIME = 8        # seconds to sample power
+MAX_WARMUP_TIME = 10   # max amount of time to wait for process to warmup
+WARMUP_TIME = 4        # need 4 seconds worth of messages for warmup to complete
 
 @dataclass
 class Proc:
@@ -24,7 +26,6 @@ class Proc:
   msgs: List[str]
   rtol: float = 0.05
   atol: float = 0.12
-  warmup: float = 6.
 
 PROCS = [
   Proc('camerad', 2.1, msgs=['roadCameraState', 'wideRoadCameraState', 'driverCameraState']),
@@ -48,6 +49,25 @@ class TestPowerDraw(unittest.TestCase):
   def tearDown(self):
     manager_cleanup()
 
+  def measure_msg_count_and_power(self, proc, max_time, msgs_expected):
+    socks = {msg: messaging.sub_sock(msg) for msg in proc.msgs}
+
+    msg_count = 0
+    start_time = time.time()
+    powers = []
+
+    while True:
+      powers.append(get_power(0.1))
+      for sock in socks.values():
+        msg_count += len(messaging.drain_sock_raw(sock))
+      if msg_count > msgs_expected or (time.time() - start_time) > max_time:
+        break
+
+    return msg_count, np.mean(powers)
+
+  def get_expected_msg_count(self, proc, time):
+    return int(sum(time * SERVICE_LIST[msg].frequency for msg in proc.msgs))
+
   @mock_messages(['liveLocationKalman'])
   def test_camera_procs(self):
     baseline = get_power()
@@ -55,31 +75,34 @@ class TestPowerDraw(unittest.TestCase):
     prev = baseline
     used = {}
     msg_counts = {}
-    for proc in PROCS:
-      socks = {msg: messaging.sub_sock(msg) for msg in proc.msgs}
-      managed_processes[proc.name].start()
-      time.sleep(proc.warmup)
-      for sock in socks.values():
-        messaging.drain_sock_raw(sock)
 
-      now = get_power(SAMPLE_TIME)
+    for proc in PROCS:
+      managed_processes[proc.name].start()
+
+      msgs_expected = self.get_expected_msg_count(proc, WARMUP_TIME)
+      msgs_received, _ = self.measure_msg_count_and_power(proc, MAX_WARMUP_TIME, msgs_expected)
+
+      with self.subTest(msg=f'warmup failed for {proc}'):
+        np.testing.assert_allclose(msgs_expected, msgs_received, rtol=.02, atol=2)
+
+      msgs_expected = self.get_expected_msg_count(proc, SAMPLE_TIME)
+      msgs_received, now = self.measure_msg_count_and_power(proc, SAMPLE_TIME, msgs_expected)
+
+      with self.subTest(msg=f'msg count failed for {proc}'):
+        np.testing.assert_allclose(msgs_expected, msgs_received, rtol=.02, atol=2)
+
+      msg_counts[proc.name] = msgs_received
       used[proc.name] = now - prev
       prev = now
-      for msg,sock in socks.items():
-        msg_counts[msg] = len(messaging.drain_sock_raw(sock))
+
+      with self.subTest(msg=f'power failed for {proc}'):
+        np.testing.assert_allclose(used[proc.name], proc.power, rtol=proc.rtol, atol=proc.atol)
 
     manager_cleanup()
 
     tab = [['process', 'expected (W)', 'measured (W)', '# msgs expected', '# msgs received']]
     for proc in PROCS:
-      cur = used[proc.name]
-      expected = proc.power
-      msgs_received = sum(msg_counts[msg] for msg in proc.msgs)
-      msgs_expected = int(sum(SAMPLE_TIME * SERVICE_LIST[msg].frequency for msg in proc.msgs))
-      tab.append([proc.name, round(expected, 2), round(cur, 2), msgs_expected, msgs_received])
-      with self.subTest(proc=proc.name):
-        np.testing.assert_allclose(msgs_expected, msgs_received, rtol=.02, atol=2)
-        np.testing.assert_allclose(cur, expected, rtol=proc.rtol, atol=proc.atol)
+      tab.append([proc.name, round(proc.power, 2), round(used[proc.name], 2), msgs_expected, msgs_received])
     print(tabulate(tab))
     print(f"Baseline {baseline:.2f}W\n")
 
